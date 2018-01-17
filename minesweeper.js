f3df86f0459487e3b4549c3fb868e1b0e18da7cc
JonnyDavis <jonnydavis39@yahoo.co.uk>
2018-01-17 17:31:02 +0000

    corrected an error in mine generation, now fully functional

--- minesweeper.js
+++ minesweeper.js
@@ -4,11 +4,13 @@ $($ => {
 	let gameBoard = $("#grid");
 	let columns = $("#columns");
 	let rows = $("#rows");
-	let maxMines = 10;
+	// 
+	let mines = $("#mines");
 	let block = $("#block");
-	let numberOfMines = $("#mines").val();
+	// tracking the current number of mines generated
+	let numberOfMines = 0;
 
-
+ // 
 
 	generateButton.on("click", function () {
 
@@ -23,6 +25,7 @@ $($ => {
 	function generateBoard() {
 		// width of each box
 		let w = 50;
+		
 		
 		for(var x = 0; x < columns.val(); x++) {
         
@@ -52,18 +55,24 @@ $($ => {
 	function generateMines() { 
 		
 		let numberOfBoxes = columns.val() * rows.val();
-
+		let maxMines = mines.val();
+		console.log(maxMines);
+		// a loop to repeat the mine generating function until the correct number of mines have been generated
 		while (numberOfMines < maxMines) {
-			
+			// a loop to apply a function for each block (div)
 			$(".block").each ((index, el) => {
+				
 				let block = $(el);
-
+				// random number generator set to the max size of the game board - used to match the index of each div to a random number so as to randomly place mines
 				let randomNum = Math.floor(Math.random() * numberOfBoxes + 1);
 				
+				// maximum number of mines condition (similar functionality as while loop above)
 				if(numberOfMines < maxMines) {
 				
+				// prevents mines from being applied to divs which already contain a mine 
 					if (!block.hasClass("mine")) {
 						
+						// 
 						if (index === randomNum) {
 		         			let mine = $("<text>x</text>");
 							block.addClass("mine");
