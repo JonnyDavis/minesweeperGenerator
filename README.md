bfab6726d77e544cdb59417a5b2c0162edf2748c
JonnyDavis <jonnydavis39@yahoo.co.uk>
2018-01-18 16:59:25 +0000

    added CSS, reset button, and updated README. Also tidied up some of the code, added comments.

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# minesweeperGeneratorFuture
+# Minesweeper Board Generator - Jonathan Davis
+
+# Planning / Thought Process
+
+1. Accept user inputs of column & row size, as well as the number of mines.
+2. Construct a grid based on the users inputs.
+3. Generate mines randomly dispersed around the grid.
+4. Calculate number of mines surrounding each empty cell.
+
+I used Javascript in conjunction with JQuery as I not only felt comfortable using this language, but also felt that the nature of the task lends itself well to JQuery / Javascript due to the mathematics & generation through functions that would be involved.
+
+## Generating the Board
+
+In order to generate the board I need to accept 'n' columns & rows and can iterate over both these numbers with for loops in order to produce the correct number of cells (empty divs). I can then position these cells using CSS.
+
+## Generating the Mines
+
+I can generate the mines by iterating over each div and utilising a randomly generated number to assign an 'X' to each div when said number equals the index. I can repeat this process until the maximum number of mines has been produced.
+
+## Setting Blank Cells to Display Number of Surrounding Mines
+
+I can take the coordinates of each cell containing mines and each blank cell and store them in seperate arrays. I can then iterate over each object in both arrays and set a condition to determine whether an object in the blank array is close to an object in the mines array.
+
+Using a counter variable to keep track of how many mines are surrounding a blank cell, i can then assign the appropriate text to that cell.
+
+# Conclusion / Possible Improvements
+
+I'm happy that the criteria of the task has been met although I feel having completed the task, that certain sections could have been done differently.
+
+- Firstly, the code is poorly optimised, particularly due to the amount of times each cell is being iterated over. This is noticable if you try to generate a large number of cells. Finding a different way to store cell coordinates could help combat this problem.
+- If i had more time I would implement some CSS, as in its current state the game is looking pretty barebones and not particularly attractive to potential users.
+- Another beneficial improvement could be to completely reset the grid should the user generate multiple times without refreshing. In its current state the new grid will generate on top the previous grid should the user not refresh the page.
--- minesweeper.js
+++ minesweeper.js
@@ -1,6 +1,7 @@
 $($ => {
 
 	let generateButton = $("#generate");
+	let resetButton = $("#reset");
 	let gameBoard = $("#grid");
 	let columns = $("#columns");
 	let rows = $("#rows");
@@ -11,34 +12,44 @@ $($ => {
 
  // 
 
+ 	resetButton.on("click", function() {
+
+	 window.location.reload();
+
+	});
+
 	generateButton.on("click", function () {
+
 
 
 		generateBoard();
 
 	});
 
+
+
 	function generateBoard() {
 		// width of each cell
 		let w = 50;
-			
+		// constructing the board by running through the number of columns * the number of rows with 2 for loops
 		for(var x = 0; x < columns.val(); x++) {
         
         	for(var y = 0; y < rows.val(); y++) {
-            	
+            	// creating a div for each unit
             	let cell = $("<div>");
             	cell.addClass("cell");
 
-            	
-            	let col = (x % columns.val());
-            	let row = Math.floor(y % rows.val());
-            	
+            	let col = x;
+            	let row = y;
+            	// shifting each cell either right or down in the grid based on col / row number * the width of the cell
             	cell.css( "left", col * w);
             	cell.css( "top", row * w);
             	
+            	// adding a class to ensure borders display correctly on the bottom right of the grid
             	if(col === columns.val() - 1)cell.addClass("right");
             	if(row === rows.val() - 1)cell.addClass("bottom");
             	
+            	// adds each cell to the grid <div> as children
             	cell.appendTo(gameBoard);
             }
 		}
@@ -51,17 +62,17 @@ $($ => {
 		
 		let numberOfBoxes = columns.val() * rows.val();
 		let maxMines = mines.val();
-		//console.log(maxMines);
+
 		// a loop to repeat the mine generating function until the correct number of mines have been generated
 		do {
-			// a loop to apply a function for each cell (div)
+			// looping over  each individual cell <div>
 			$(".cell").each ((index, el) => {
 				
 				let cell = $(el);
-				// random number generator set to the max size of the game board - used to match the index of each div to a random number so as to randomly place mines
+				// random number generator set to the max size of the game board - used to match the index of each div to a random number so as to randomly place mines when the two equal
 				let randomNum = Math.floor(Math.random() * numberOfBoxes + 1);
 				
-				// maximum number of mines condition (similar functionality as while loop above)
+				// maximum number of mines condition (ensures the number of mines does not exceed that set by the user)
 				if(numberOfMines < maxMines) {
 				
 				// prevents mines from being applied to divs which already contain a mine 
@@ -74,98 +85,112 @@ $($ => {
 		         			mine.appendTo(cell);
 		         			numberOfMines += 1;
 
-		         			//console.log(numberOfMines);
+		         			
 						}
 					}
 				}
 			})
 		}
-		
+		// similar to the condition above, ensures the number of mines does not exceed that set by the user
 		while (numberOfMines < maxMines);
 
-		//console.log(numberOfMines);
-		//console.log(maxMines);
-
+		// ensures the next function only runs once the correct number of mines has been generated
 		if (numberOfMines = maxMines) {
-			checkMineDistance();
+			checkMineLocation();
 		}
 	
 	}
 
 
-	function checkMineDistance() {
+	function checkMineLocation() {
 
-		let bombCells = [];
-		let notBombCells = [];
+		let mineCells = [];
+		let notMineCells = [];
 		
+		// iterates over each cell <div> and stores their coordinates in two seperate arrays for mine coordinates, and empty cell coordinates
 		$(".cell").each ((index, el) => {
 			let cell = $(el);
 
 			if (cell.hasClass("mine")) {
-			
-				bombCells.push(cell.position());
+				
+				// cell.position() returns the top and left css values of each cell
+				mineCells.push(cell.position());
 			}
 
 			if (!cell.hasClass("mine")) {
 
-				notBombCells.push(cell.position()); 
+				notMineCells.push(cell.position()); 
 			}
 
 		})
-
-		setMineDistance(bombCells, notBombCells);
+		// passes both arrays into the next function
+		setMineDistance(mineCells, notMineCells);
 	
 	}
 
 
-	function setMineDistance(bombArr, notBombArr) {
-		let testBomb = { top: 100, left: 50};
-		let testCell = { top: 100, left: 150};
-		// console.log(bombArr);
-		// console.log(notBombArr);
+	function setMineDistance(mineArr, notMineArr) {
 
-		$.each(notBombArr, function(i, value) {
+		// $.each accesses each object in an array i.e. notMineArr
+		$.each(notMineArr, function(i, value) {
+			
 			let blankObj = value;
+
+			// a counter to keep track of how many mines are nearby, set to 0 for each object
 			blankObj.counter = 0;
-			console.log(blankObj);
 
-			$.each(bombArr, function(i, obj) {
-				let bombObj = obj;
+			$.each(mineArr, function(i, obj) {
+				
+				let mineObj = obj;
 
-				//console.log(bombObj.top);
-				if (bombObj.top === blankObj.top && Math.abs(bombObj.left - blankObj.left) === 50) {
+				// a set of conditions that compare the positions of each blank cell to each mine
+				// if any condition is met, a mine must be nearby, and the counter is increased by one.
+				if (mineObj.top === blankObj.top && Math.abs(mineObj.left - blankObj.left) === 50) {
 				  	blankObj.counter += 1;
 				}
 
-				if (bombObj.left === blankObj.left && Math.abs(bombObj.top - blankObj.top) === 50) {
+				if (mineObj.left === blankObj.left && Math.abs(mineObj.top - blankObj.top) === 50) {
 				  	blankObj.counter += 1;
 					
 				}
 
-				if (Math.abs(bombObj.top - blankObj.top) === 50 && Math.abs(bombObj.left - blankObj.left) === 50) {
+				if (Math.abs(mineObj.top - blankObj.top) === 50 && Math.abs(mineObj.left - blankObj.left) === 50) {
 				  	blankObj.counter += 1;
 					
 				}
 			})
 		})
 
-		//console.log(notBombArr);
-
+		// iterating over each cell for the purpose of applying the object counter to the cell, should that counter exist
 		$(".cell").each ((index, el) => {
 			let cell = $(el);
 
-			//console.log(cell.position().top);
-			$.each(notBombArr, function(i, obj) {
+			$.each(notMineArr, function(i, obj) {
 
-				//console.log(obj.top);
+				
 				if (obj.counter) {
 
-					//console.log(obj);
+					// matches the position of the cells to the position stored in the object to ensure that the correct counter is applied to the correct object
 					if (cell.position().top === obj.top && cell.position().left === obj.left) {
+						
 						let mineDistance = $("<text>"+obj.counter+"</text>");
 		         			mineDistance.appendTo(cell);
 		         			
+
+		         		if (obj.counter === 1) {
+						cell.addClass("one")
+						} else if (obj.counter === 2) {
+							cell.addClass("two")
+						} else if (obj.counter === 3) {
+							cell.addClass("three")
+						} else if (obj.counter === 4) {
+							cell.addClass("four")
+						} else if (obj.counter === 5) {
+							cell.addClass("five")
+						}
 					}
+
+
 				}
 			})
 		})
